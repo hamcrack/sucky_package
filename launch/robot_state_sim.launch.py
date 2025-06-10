ab2c2ba3cb9f1d55d04f9cda7a5aaafca5014366
hamcrack <hamishcrackett@gmail.com>
2025-06-10 17:14:08 +0200

    wheeled robot base urdf finished

--- launch/robot_state_sim.launch.py
+++ launch/robot_state_sim.launch.py
@@ -8,44 +8,49 @@ from launch_ros.actions import Node
 import xacro
 
 def generate_launch_description():
-    # Declare the world argument
-    declare_world_cmd = DeclareLaunchArgument(
-        'world',
-        default_value=os.path.join(get_package_share_directory('gazebo_ros'), 'worlds', 'empty.world'),
-        description='Full path to the world model file to load'
-    )
+    # # Declare the world argument
+    # declare_world_cmd = DeclareLaunchArgument(
+    #     'world',
+    #     default_value=os.path.join(get_package_share_directory('gazebo_ros'), 'worlds', 'empty.world'),
+    #     description='Full path to the world model file to load'
+    # )
 
-    world = LaunchConfiguration('world')
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
     pkg_name = 'sucky_package'
     file_subpath = 'description/sucky_robot.urdf.xacro'
     xacro_file = os.path.join(get_package_share_directory(pkg_name), file_subpath)
-    robot_description_raw = xacro.process_file(xacro_file).toxml()
+    robot_description_config = xacro.process_file(xacro_file)
+    # robot_description_raw = xacro.process_file(xacro_file).toxml()
 
+    # Create a robot_state_publisher node
+    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[{'robot_description': robot_description_raw, 'use_sim_time': True}]
+        parameters=[params]
     )
 
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
-        ),
-        launch_arguments={'world': world}.items()
-    )
+    # gazebo = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(
+    #         os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
+    #     ),
+    #     launch_arguments={'world': world}.items()
+    # )
 
-    spawn_entity = Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
-        arguments=['-topic', 'robot_description', '-entity', 'my_bot'],
-        output='screen'
-    )
+    # spawn_entity = Node(
+    #     package='gazebo_ros',
+    #     executable='spawn_entity.py',
+    #     arguments=['-topic', 'robot_description', '-entity', 'my_bot'],
+    #     output='screen'
+    # )
 
     return LaunchDescription([
-        declare_world_cmd,
-        gazebo,
-        node_robot_state_publisher,
-        spawn_entity
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use sim time if true'),
+
+        node_robot_state_publisher
     ])
