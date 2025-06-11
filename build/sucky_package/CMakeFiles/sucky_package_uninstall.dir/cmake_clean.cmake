15448af0941f7e3dbf51243569a118e63cec39f7
hamcrack <hamishcrackett@gmail.com>
2025-06-11 18:42:15 +0200

    removed logs

--- build/sucky_package/CMakeFiles/sucky_package_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/sucky_package_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sucky_package_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
